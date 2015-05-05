db0a5ca6e72d2a3f6d7f354e9812b136ac88b5f2
alselgar <alselgar@gmail.com>
2015-05-05 12:24:43 +0200

    Added Bomb class

---
+++ include/Bomb.h
@@ -0,0 +1,27 @@
+#ifndef BOMB_H
+#define BOMB_H
+
+#include "SFML/Graphics.hpp"
+#include "Ship.h"
+#include "Bullet.h"
+
+class Bomb
+{
+public:
+    Bomb(int x, int y);
+    virtual ~Bomb();
+    void draw(sf::RenderWindow &window);
+    int getX();
+    int getY();
+    sf::Vector2f getDimensions();
+    sf::FloatRect getSpriteGlobalBounds();
+    bool intersects(Ship &s);
+    bool intersects(Bullet &b);
+    void changeSprite();
+
+protected:
+    sf::Texture yellow, red;
+    sf::Sprite sprite;
+};
+
+#endif // BOMB_H
---
+++ src/Bomb.cpp
@@ -0,0 +1,56 @@
+#include "Bomb.h"
+
+Bomb::Bomb(int x, int y) {
+    if(!yellow.loadFromFile("bomb1.png")) {
+        std::cout << "Error loading bomb1 texture" << std::endl;
+    }
+
+     if(!red.loadFromFile("bomb2.png")) {
+        std::cout << "Error loading bomb2 texture" << std::endl;
+    }
+    sprite.setTexture(yellow);
+    sprite.setPosition(x, y);
+}
+
+
+Bomb::~Bomb() {
+}
+
+
+void Bomb::draw(sf::RenderWindow &window) {
+    window.draw(sprite);
+}
+
+
+
+int Bomb::getX() {
+    return sprite.getPosition().x;
+}
+
+int Bomb::getY() {
+    return sprite.getPosition().y;
+}
+
+sf::Vector2f Bomb::getDimensions() {
+    sf::FloatRect fr = sprite.getGlobalBounds();
+    sf::Vector2f dimensions(fr.width, fr.height);
+    return dimensions;
+}
+
+sf::FloatRect Bomb::getSpriteGlobalBounds() {
+    return sprite.getGlobalBounds();
+}
+
+bool Bomb::intersects(Ship &s) {
+    return sprite.getGlobalBounds().intersects(s.getSpriteGlobalBounds());
+}
+
+bool Bomb::intersects(Bullet &b) {
+    return sprite.getGlobalBounds().intersects(b.getCircleShapeGlobalBounds());
+}
+
+void Bomb::changeSprite() {
+    if(sprite.getTexture() == &yellow)
+        sprite.setTexture(red);
+    else sprite.setTexture(yellow);
+}
