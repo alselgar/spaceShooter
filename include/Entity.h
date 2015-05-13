dff722a0b14ddc9b169e929a723f798150d98210
alselgar <alselgar@gmail.com>
2015-05-13 10:51:13 +0200

    Added Entity class

---
+++ include/Entity.h
@@ -0,0 +1,25 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "SFML/Graphics.hpp"
+
+class Entity
+{
+    public:
+        Entity(char *spritePath, int x, int y);
+        virtual ~Entity();
+        void draw(sf::RenderWindow &window);
+        int getX();
+        int getY();
+        sf::Vector2f getDimensions();
+        sf::FloatRect getSpriteGlobalBounds();
+        bool intersects(Entity &e);
+
+    protected:
+        sf::Texture mainTexture;
+        sf::Sprite mainSprite;
+    private:
+
+};
+
+#endif // ENTITY_H
---
+++ src/Entity.cpp
@@ -0,0 +1,47 @@
+#include "Entity.h"
+#include <iostream>
+
+Entity::Entity(char *spritePath, int x, int y)
+{
+    if(!mainTexture.loadFromFile(spritePath)) {
+        std::cout << "Error loading " << spritePath << std::endl;
+    }
+    mainSprite.setTexture(mainTexture);
+    mainSprite.setPosition(x,y);
+
+}
+
+Entity::~Entity()
+{
+    //dtor
+}
+
+void Entity::draw(sf::RenderWindow &window) {
+    window.draw(mainSprite);
+}
+
+int Entity::getX() {
+    return mainSprite.getPosition().x;
+}
+
+int Entity::getY() {
+    return mainSprite.getPosition().y;
+}
+
+sf::Vector2f Entity::getDimensions() {
+    sf::FloatRect fr = mainSprite.getGlobalBounds();
+    sf::Vector2f dimensions(fr.width, fr.height);
+    return dimensions;
+}
+
+sf::FloatRect Entity::getSpriteGlobalBounds() {
+    return mainSprite.getGlobalBounds();
+}
+
+bool Entity::intersects(Entity &e) {
+    return mainSprite.getGlobalBounds().intersects(e.getSpriteGlobalBounds());
+}
+
+
+
+
